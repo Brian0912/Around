bf99da73b4d1f20eee2d025c6cc3d4407d7f5204
Brian_Huang <brian800912@gmail.com>
2019-08-25 07:50:17 +0000

    update github

--- startServer.sh
+++ startServer.sh
@@ -1,7 +1,7 @@
 sudo systemctl enable elasticsearch
 sudo systemctl start elasticsearch
 
-export GOOGLE_APPLICATION_CREDENTIALS="/home/bh800912/Documents/Around/Around-034ae903e269.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/home/bh800912/Documents/Around/file.json"
 echo $GOOGLE_APPLICATION_CREDENTIALS
 
 go run main.go user.go
